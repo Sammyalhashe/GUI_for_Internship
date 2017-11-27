0f78ddb4b2dbe557feebe6f0885dc0da6c3f3530
Sammy Al-Hashemi <sammy.el-hashemi@microsemi.net>
2017-11-27 13:17:51 -0800

    Literally Just a sports odds function -> percent

---
+++ SportsOdds.py
@@ -0,0 +1,34 @@
+def odds(string):
+    nums = []
+    build = ""
+    i = 0
+    for char in string:
+        try:
+            int(char)
+            build += char
+        except ValueError:
+            if(char == "/"):
+                nums.append(int(build.strip()))
+                build = ""
+        if(i == len(string)-1):
+            nums.append(int(build.strip()))
+        i += 1
+
+    return (nums[1]/(nums[0]+nums[1]))
+
+
+def main():
+    FedererOdds = "11  /           4"
+    NadalOdds = "3/1  "
+    DjokovicOdds = "7     /        2"
+    MurrayOdds = DjokovicOdds
+    fed = odds(FedererOdds)
+    nad = odds(NadalOdds)
+    djok = odds(DjokovicOdds)
+    murr = odds(MurrayOdds)
+    print("%f percent"%(fed*100))
+    print("%f percent"%(nad*100))
+    print("%f percent"%(djok*100))
+    print("%f percent"%(murr*100))
+
+main()
