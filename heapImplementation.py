9bdebc68f805d02f077561adc963643779e01a1a
Sammyalhashe <sammy.alhashemi@mail.utoronto.ca>
2017-12-03 17:14:01 -0800

    Added heapsort to Heap implementation

--- heapImplementation.py
+++ heapImplementation.py
@@ -22,7 +22,9 @@ class Heap(object):
         self.heap[i2] = temp
 
     def restoreHeap(self, size):
-
+        """
+        restores min heap property (use only for insert)
+        """
         while (size // 2 > 0):
             if(self.heap[size // 2] > self.heap[size]):
                 self.swap(size, size // 2)
@@ -59,6 +61,14 @@ class Heap(object):
             return smallest
 
     def buildHeap(self, array):
+        """
+        @brief      Builds a heap.
+
+        @param      self   The object
+        @param      array  The array
+
+        @return     The heap.
+        """
         index = len(array) // 2
         self.current_size = len(array)
         self.heap = [0] + array
@@ -66,10 +76,58 @@ class Heap(object):
             self.bubbleDown(index)
             index -= 1
 
+    def satisfyMinHeapProperty(self, index, current_size):
+        """
+        @brief      recursive func satisfying min heap property
+
+        @param      self          The object
+        @param      index         The index
+        @param      current_size  The current size
+
+        @return     { no return val; alters self.heap }
+        """
+        LC = 2 * index
+        RC = 2 * index + 1
+        smallest = index
+        if(LC <= current_size and self.heap[LC] < self.heap[index]):
+            smallest = LC
+        if(RC <= current_size and self.heap[RC] < self.heap[smallest]):
+            smallest = RC
+        if smallest != index:
+            self.swap(index, smallest)
+            self.satisfyMinHeapProperty(smallest, current_size)
+
+    def HeapSort(self, reverse=True):
+        """
+        @brief      { function_description }
+
+        @param      self     The object
+        @param      reverse  If true sorted in descending order
+
+        @return     { sorted array }
+        """
+        # array to be returned
+        sorted_arr = []
+        # defined in constructor for heap
+        current_size = self.current_size
+        while current_size != 0:
+            self.swap(1, current_size)
+            sorted_arr.append(self.heap[current_size])
+            current_size -= 1
+            self.satisfyMinHeapProperty(1, current_size)
+        # restore the object heap
+        self.buildHeap(sorted_arr)
+        return (sorted_arr if reverse else sorted_arr[::-1])
+
 
 if __name__ == '__main__':
+    """
+    main block to execute tests
+    """
     test = [1, 5, 3, 4, 6, 7, 5, 3]
     print(test)
     Heap = Heap()
     Heap.buildHeap(test)
     print(Heap.heap)
+    sorted_arr = Heap.HeapSort()
+    print(sorted_arr)
