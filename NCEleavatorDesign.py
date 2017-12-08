f7a5709150346f76be18bca6485dbf909eb3f407
Sammy Al-Hashemi <sammy.el-hashemi@microsemi.net>
2017-12-08 13:39:56 -0800

    addded docstrings to be filled in for elevator

--- NCEleavatorDesign.py
+++ NCEleavatorDesign.py
@@ -1,22 +1,45 @@
-from Minheap import minHeap
+from Maxheap import maxHeap
 import time
 
 
 class Building(object):
-    """docstring for Building"""
+    """[summary]
+
+    [description]
+    """
 
     def __init__(self, N, NumElev):
         super().__init__()
         self.N = N
         self.NumElev = NumElev
-        self._Elevator_List = minHeap(key=None)
+        self._Elevator_List = maxHeap(key=None)
 
     @property
     def N(self):
+        """[summary]
+
+        [description]
+
+        Returns:
+            [type] -- [description]
+        """
         return self._N
 
     @N.setter
     def N(self, N):
+        """[summary]
+
+        [description]
+
+        Decorators:
+            N.setter
+
+        Arguments:
+            N {[type]} -- [description]
+
+        Raises:
+            ValueError -- [description]
+        """
         if N <= 0:
             raise ValueError
             print("Building must at least have 1 floor")
@@ -25,25 +48,66 @@ class Building(object):
 
     @property
     def NumElev(self):
+        """[summary]
+
+        [description]
+
+        Returns:
+            [type] -- [description]
+        """
         return self.__NumElev
 
     @NumElev.setter
     def NumElev(self, NumElev):
+        """[summary]
+
+        [description]
+
+        Decorators:
+            NumElev.setter
+
+        Arguments:
+            NumElev {[type]} -- [description]
+        """
         self._NumElev = NumElev
         for i in range(self._NumElev):
             self._Elevator_List.buildHeap(
                 [Elevator(self, elevator_num=i) for i in range(self._NumElev)], key=lambda x: x.elevator_num)
 
     def ElevatorCall(self, floor):
+        """[summary]
+
+        [description]
+
+        Arguments:
+            floor {[type]} -- [description]
+        """
         self._Elevator_List.reconstructHeap(key=lambda x: x.FS)
-        # Need a max heap here
+        self._Elevator_List.heap[1].move(
+            floor - self._Elevator_List.heap[1].floor)  # elevator with max FS
         pass
 
 
 class Elevator(object):
-    """docstring for Elevator"""
+    """[summary]
+
+    [description]
+    """
 
     def __init__(self, building, elevator_num, floor=0, motion=0, people=0):
+        """[summary]
+
+        [description]
+
+        Arguments:
+            building {[type]} -- [description]
+            elevator_num {[type]} -- [description]
+
+        Keyword Arguments:
+            floor {number} -- [description] (default: {0})
+            motion {number} -- [description] (default: {0})
+            people {number} -- [description] (default: {0})
+        """
         super().__init__()
         self.floor = floor
         self.motion = motion
@@ -53,18 +117,52 @@ class Elevator(object):
 
     @property
     def elevator_num(self):
+        """[summary]
+
+        [description]
+
+        Returns:
+            [type] -- [description]
+        """
         return self._elevator_num
 
     @elevator_num.setter
     def elevator_num(self, elevator_num):
+        """[summary]
+
+        [description]
+
+        Decorators:
+            elevator_num.setter
+
+        Arguments:
+            elevator_num {[type]} -- [description]
+        """
         self._elevator_num = elevator_num
 
     @property
     def building(self):
+        """[summary]
+
+        [description]
+
+        Returns:
+            [type] -- [description]
+        """
         return self._building
 
     @building.setter
     def building(self, building):
+        """[summary]
+
+        [description]
+
+        Decorators:
+            building.setter
+
+        Arguments:
+            building {[type]} -- [description]
+        """
         if building is None:
             print("No building assigned")
         else:
@@ -72,22 +170,67 @@ class Elevator(object):
 
     @property
     def floor(self):
+        """[summary]
+
+        [description]
+
+        Returns:
+            [type] -- [description]
+        """
         return self._floor
 
     @floor.setter
     def floor(self, floor):
+        """[summary]
+
+        [description]
+
+        Decorators:
+            floor.setter
+
+        Arguments:
+            floor {[type]} -- [description]
+        """
         self._floor = floor
 
     @property
     def motion(self):
+        """[summary]
+
+        [description]
+
+        Returns:
+            [type] -- [description]
+        """
         return self._motion
 
     @motion.setter
     def motion(self, motion):
+        """[summary]
+
+        [description]
+
+        Decorators:
+            motion.setter
+
+        Arguments:
+            motion {[type]} -- [description]
+        """
         self._motion = motion
 
     @property
     def FS(self, floor, call):
+        """[summary]
+
+        [description]
+
+        Arguments:
+            floor {[type]} -- [description]
+            call {[type]} -- [description]
+
+        Returns:
+            [type] -- [description]
+        """
         agreement = 1 if(self.motion >= 0 and call >= 0) else - \
             1 if (self.motion < 0 and call < 0) else 0
         convinience = ((floor >= self.floor) * 1 if agreement >
@@ -109,6 +252,16 @@ class Elevator(object):
         return fs
 
     def move(self, floors):
+        """[summary]
+
+        [description]
+
+        Arguments:
+            floors {[type]} -- [description]
+
+        Raises:
+            ValueError -- [description]
+        """
         if not (self.floor + floors > self.building.N or self.floor + floors < 0):
             self.floor += floors
 
