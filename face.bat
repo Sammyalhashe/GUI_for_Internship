f1037fb1814009822cf0755101ab7aa6c385d91f
Sammy Al-Hashemi <sammy.el-hashemi@microsemi.net>
2017-12-08 18:04:33 -0800

    batch file changes

--- face.bat
+++ face.bat
@@ -1,3 +1,5 @@
 @echo off
+set searchq=%1%2
 echo Opening Facebook...
-chrome https://www.facebook.com
+if "%searchq%" == "" (chrome https://www.facebook.com/) else (chrome https://www.facebook.com/search/top/?q="%1 %2")
+
