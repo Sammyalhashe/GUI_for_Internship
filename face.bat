c6a883106d9c903a9b4ce29586146ffa20e95cc3
Sammy Al-Hashemi <sammy.el-hashemi@microsemi.net>
2018-01-05 15:24:34 -0800

    work

--- face.bat
+++ face.bat
@@ -1,10 +1,6 @@
-<<<<<<< HEAD
-@echo off
-set searchq=%1%2
-echo Opening Facebook...
-if "%searchq%" == "" (chrome https://www.facebook.com/) else (chrome https://www.facebook.com/search/top/?q="%1 %2")
 
-=======
+
+
 @ECHO off
 ::set searchq=%1%2
 set /P prompt= Want main page? [y/n]
@@ -22,4 +18,4 @@ set /P search= Search Person:
 (chrome https://www.facebook.com/search/top/?q="%search%")
 
 :END
->>>>>>> 3baf57ceecff4eee51d0142a2791cab22e0a7317
+
